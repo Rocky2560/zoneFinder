d5d681dd565e83716bc5f59c8df86d0a8b4c6262
rocky2560 <sudipmanandhar2560@gmail.com>
2025-02-23 23:02:16 +0000

    First commit

---
+++ src/main/java/com/example/zoneFinder/ZoneFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoneFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoneFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoneFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zoneFinder/ZoneFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoneFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoneFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
